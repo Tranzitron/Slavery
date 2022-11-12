044119179050579b08a116bddd6da8ee4c864182
Tranzitron <raphaelvallee.1@gmail.com>
2022-11-12 13:23:35 -0500

    Updated plugin.yml added InteractListener

---
+++ src/main/java/us/babey/slavery/Listeners/InteractListener.java
@@ -0,0 +1,14 @@
+package us.babey.slavery.Listeners;
+
+import org.bukkit.event.Listener;
+import us.babey.slavery.Main;
+
+public class InteractListener implements Listener {
+
+    private Main main;
+    public InteractListener(Main main){
+        this.main = main;
+    }
+
+
+}
